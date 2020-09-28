541b227c9a260b24138f3afe66cf6a1d848328f7
Michael Howitz <mh@gocept.com>
2020-09-28 10:08:05 +0200

    Fox version number.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ README = _boundary.join([
 
 setup(
     name='Products.%s' % NAME,
-    version='3.1.0',
+    version='3.1.0.dev0',
     description='Uid product for the Zope Content Management Framework',
     long_description=README,
     classifiers=[
