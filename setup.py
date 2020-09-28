4427dc79811eca4c20388d03b7893d911766f0c3
Michael Howitz <mh@gocept.com>
2020-09-28 10:22:37 +0200

    Drop support for ``python setup.py test``. (#15)

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ README = _boundary.join([
 
 setup(
     name='Products.%s' % NAME,
-    version='3.0.3.dev0',
+    version='3.1.0.dev0',
     description='Uid product for the Zope Content Management Framework',
     long_description=README,
     classifiers=[
@@ -48,24 +48,19 @@ setup(
     include_package_data=True,
     namespace_packages=['Products'],
     zip_safe=False,
-    setup_requires=[
-        'eggtestinfo',
-    ],
     install_requires=[
         'Products.CMFCore >= 2.4.0dev',
         'Products.ZCatalog >= 4.1.1',
         'Zope',
         'setuptools',
     ],
-    tests_require=[
-        'zope.testing >= 3.7.0',
-    ],
-    test_loader='zope.testing.testrunner.eggsupport:SkipLayers',
-    test_suite='Products.%s.tests' % NAME,
+    extras_require={
+        'test': [
+            'zope.testing >= 3.7.0',
+        ]
+    },
     entry_points="""
     [zope2.initialize]
     Products.%s = Products.%s:initialize
-    [distutils.commands]
-    ftest = zope.testing.testrunner.eggsupport:ftest
     """ % (NAME, NAME),
 )
