0ba8964dba0b2fdd9d3a74acf6485adb68a004b4
Jens Vagelpohl <jens@plyp.com>
2023-02-01 09:38:22 +0100

    - prepare release 4.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ README = _boundary.join([
 
 setup(
     name='Products.%s' % NAME,
-    version='4.0.dev0',
+    version='4.0',
     description='Uid product for the Zope Content Management Framework',
     long_description=README,
     classifiers=[
